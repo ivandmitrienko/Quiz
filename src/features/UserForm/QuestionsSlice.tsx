8e52e81374dd84fe779a5cd0335624c1e4f1e496
ivandmitrienko <oneblackcrystal@gmail.com>
2024-06-23 20:18:06 +0300

    initial commit

--- src/features/UserForm/QuestionsSlice.tsx
+++ src/features/UserForm/QuestionsSlice.tsx
@@ -6,10 +6,9 @@ const initialState: IQuizConfigState[] = [];
 export const createSliceQuestions = createAsyncThunk(
   '@@questions/createQuestions',
   async (config: IQuizConfigState, { dispatch }) => {
-    console.log(config.category);
-    const { difficulty, type, time, category, numberOfQuestions } = config;
+    const { difficulty, type, category } = config;
     const res = await fetch(
-      `http://localhost:3000/questions?category=${category}&type=${type}&_difficulty=${difficulty}`,
+      `http://localhost:3000/questions?category=${category}&type=${type}&difficulty=${difficulty}`,
     );
     const data = await res.json();
 
