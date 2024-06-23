5b303304652ff3c1b62af702493089a6b38cf0f7
ivandmitrienko <oneblackcrystal@gmail.com>
2024-06-23 22:19:41 +0300

    initial commit

--- src/features/UserForm/QuestionsSlice.tsx
+++ src/features/UserForm/QuestionsSlice.tsx
@@ -1,36 +1,44 @@
 import { PayloadAction, createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { IQuizConfigState } from '../../types/types';
-
-const initialState: IQuizConfigState[] = [];
+import { IQuizConfigState, IStructureOfQuestions } from '../../types/types';
 
 export const createSliceQuestions = createAsyncThunk(
   '@@questions/createQuestions',
   async (config: IQuizConfigState, { dispatch }) => {
+    dispatch(saveStructureTest(config));
     const { difficulty, type, category, quantityOfQuestions } = config;
     const res = await fetch(
       `http://localhost:3000/questions/${type}/${category}`,
     );
-    const data = await res.json();
+    const data: IStructureOfQuestions[] = await res.json();
 
-    /*here use filter method because json-server doesn't support multiple filter*/
-    console.log(data);
-    const questions = data.filter(
-      (e: IQuizConfigState) => e.difficulty === difficulty,
-    );
+    /*here use filter  and slice methods because json-server doesn't support multiple filter*/
+    const questions = data
+      .filter((e) => e.difficulty === difficulty)
+      .slice(0, quantityOfQuestions);
 
-    console.log(questions);
+    return questions;
   },
 );
+
+const initialState = {
+  confiqTest: [] as IQuizConfigState[],
+  dataForTest: [] as IStructureOfQuestions[],
+};
 
 const questionsSlice = createSlice({
   name: '@@questions',
   initialState,
   reducers: {
-    getQuestions: (state, action: PayloadAction<IQuizConfigState>) => {
-      state.push(action.payload);
+    saveStructureTest: (state, action: PayloadAction<IQuizConfigState>) => {
+      state.confiqTest.push(action.payload);
     },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(createSliceQuestions.fulfilled, (state, action) => {
+      state.dataForTest = action.payload;
+    });
   },
 });
 
-export const { getQuestions } = questionsSlice.actions;
+export const { saveStructureTest } = questionsSlice.actions;
 export const questionsReducer = questionsSlice.reducer;
